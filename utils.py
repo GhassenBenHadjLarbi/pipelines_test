17e1e87c230a8ed7b19034c75a27e1cc4673b0d7
Victor Sonck <victor.sonck@gmail.com>
2022-03-15 12:04:58 +0100

    remove pycache and use helper functions instead

--- utils.py
+++ utils.py
@@ -1,25 +1,26 @@
-import time
-
-
 def download():
+    import time
     print("Downloading data...")
     time.sleep(10)
     return 1
 
 
 def merge(arg, arg2):
+    import time
     print("Merging data...")
     time.sleep(1)
     return 1
 
 
 def transform(arg):
+    import time
     print("Transforming data...")
     time.sleep(4)
     return 1
 
 
 def train(arg):
+    import time
     print("Training LightGBM model...")
     time.sleep(9)
     return 0.89

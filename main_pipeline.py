fd65194820fdeaa68de1824d1609c98613d8408b
Victor Sonck <victor.sonck@gmail.com>
2022-03-15 10:56:03 +0100

    add requirements file

--- main_pipeline.py
+++ main_pipeline.py
@@ -1,7 +1,7 @@
 from clearml import PipelineDecorator
 
 
-@PipelineDecorator.component(cache=False)
+@PipelineDecorator.component(cache=False, repo='git@github.com:thepycoder/pipelines_test.git', repo_branch='main')
 def download_data():
     # We want to import our packages INSIDE the function, so the agent knows what libraries to use when this function
     # becomes an isolated pipeline step
@@ -10,21 +10,21 @@ def download_data():
     return raw_data
 
 
-@PipelineDecorator.component(cache=False)
+@PipelineDecorator.component(cache=False, repo='git@github.com:thepycoder/pipelines_test.git', repo_branch='main')
 def merge_data(raw_data, second_data_source="s3://second_data_source"):
     from utils import merge
     merged_data = merge(raw_data, second_data_source)
     return merged_data
 
 
-@PipelineDecorator.component(cache=False)
+@PipelineDecorator.component(cache=False, repo='git@github.com:thepycoder/pipelines_test.git', repo_branch='main')
 def transform_data(merged_data):
     from utils import transform
     transformed_data = transform(merged_data)
     return transformed_data
 
 
-@PipelineDecorator.component(cache=False)
+@PipelineDecorator.component(cache=False, repo='git@github.com:thepycoder/pipelines_test.git', repo_branch='main')
 def train_model(transformed_data):
     from utils import train
     accuracy = train(transformed_data)

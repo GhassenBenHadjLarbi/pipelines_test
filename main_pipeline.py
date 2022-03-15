c620da91a2459c8d98cfc307f01a815a3d7275f4
Victor Sonck <victor.sonck@gmail.com>
2022-03-15 13:40:50 +0100

    Reset to using repo argument

--- main_pipeline.py
+++ main_pipeline.py
@@ -1,34 +1,37 @@
 from clearml import PipelineDecorator
-from utils import download, merge, transform, train
 
 
-@PipelineDecorator.component(cache=False, helper_functions=[download])
+@PipelineDecorator.component(cache=False, repo='git@github.com:thepycoder/pipelines_test.git', repo_branch='main')
 def download_data():
     # We want to import our packages INSIDE the function, so the agent knows what libraries to use when this function
     # becomes an isolated pipeline step
+    from utils import download
     raw_data = download()
     return raw_data
 
 
-@PipelineDecorator.component(cache=False, helper_functions=[merge])
+@PipelineDecorator.component(cache=False, repo='git@github.com:thepycoder/pipelines_test.git', repo_branch='main')
 def merge_data(raw_data, second_data_source="s3://second_data_source"):
+    from utils import merge
     merged_data = merge(raw_data, second_data_source)
     return merged_data
 
 
-@PipelineDecorator.component(cache=False, helper_functions=[transform])
+@PipelineDecorator.component(cache=False, repo='git@github.com:thepycoder/pipelines_test.git', repo_branch='main')
 def transform_data(merged_data):
+    from utils import transform
     transformed_data = transform(merged_data)
     return transformed_data
 
 
-@PipelineDecorator.component(cache=False, helper_functions=[train])
+@PipelineDecorator.component(cache=False, repo='git@github.com:thepycoder/pipelines_test.git', repo_branch='main')
 def train_model(transformed_data):
+    from utils import train
     accuracy = train(transformed_data)
     return accuracy
 
 
-@PipelineDecorator.pipeline(name="ETL Pipeline", project="Pipeline Getting Started", version="0.1")
+@PipelineDecorator.pipeline(name="ETL Pipeline", project="Pipeline Examples", version="0.1")
 def main(data_query, data_location):
     raw_data = download_data()
     merged_data = merge_data(raw_data)
@@ -41,5 +44,4 @@ def main(data_query, data_location):
 if __name__ == "__main__":
     PipelineDecorator.set_default_execution_queue('default')
     # PipelineDecorator.run_locally()
-    # print(ScriptInfo.get()[0].script)
     main("SELECT * FROM customers", "s3://my_data_bucket")
